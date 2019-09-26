c4a8f469d45430481cca6c43999dc809538e00eb
Ryan Allen <rallen3882@gmail.com>
2019-09-26 14:30:37 -0400

    Adding deploy script and login button to home page

---
+++ deploy.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+aws s3 cp --recursive dist/ s3://minecraft.ryanallen.ninja
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,12 @@
 <template>
   <div id="home">
     <Banner/>
-    Home Page!
+    <a href="https://minecraft-ryanallen-ninja.auth.us-east-2.amazoncognito.com/login?client_id=1l9ec2re465bo5rc3d6ihlcrla&response_type=code&redirect_uri=https://minecraft.ryanallen.ninja">
+        <div class="button-login">
+            Log In Now!
+        </div>
+    </a>
+    <div>{{responseMessage}}</div>
   </div>
 </template>
 
@@ -11,10 +16,26 @@ export default {
     name: 'Home',
     components: {
         Banner
+    },
+    data() {
+        return {
+            responseMessage: "Empty"
+        };
+    },
+    methods: {
+        loginFunction: async function () {
+            // const resp = await fetch('https://httpbin.org/headers');
+            // const data = await resp.json();
+        }
     }
-}
+}   
 </script>
 
 <style>
+.button-login {
+  padding: 10px 30px;
+  width: 150px;
+  background-color: chartreuse;
+}
 
 </style>
