03768810cdaae811856dc85a051a568005d9f3fa
Ryan Allen <rallen3882@gmail.com>
2019-09-26 17:00:24 -0400

    Extracting auth code from dashboard and using it to get a cognito token

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -25,7 +25,18 @@ export default {
     },
     methods: {
         getCognitoToken: async function () {
-            const resp = await fetch('https://httpbin.org/headers');
+
+            const authCode = this.$route.query.code;
+
+            alert("code is: " + authCode);
+
+            const resp = await fetch('https://minecraft-ryanallen-ninja.auth.us-east-2.amazoncognito.com/oauth2/token', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                body: "grant_type=authorization_code&client_id=1l9ec2re465bo5rc3d6ihlcrla&code="+authCode+"&redirect_uri=https%3A%2F%2Feager-jang-9f2469.netlify.com%2Fdashboard"
+            });
             const headers = await resp.json();
             this.authenticated=true;
             this.cognitoToken=headers;
